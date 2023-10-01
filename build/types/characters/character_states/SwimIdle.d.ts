bac1d5865784cedc11e02dc2e6c6bd64b4c991dd
Inthenew <38168276+Inthenew@users.noreply.github.com>
2023-09-30 23:37:09 -0400

    Add files via upload

---
+++ build/types/characters/character_states/SwimIdle.d.ts
@@ -0,0 +1,6 @@
+import { CharacterStateBase } from './_stateLibrary';
+import { Character } from '../Character';
+export declare class SwimIdle extends CharacterStateBase {
+    constructor(character: Character);
+    update(timeStep: number): void;
+}
