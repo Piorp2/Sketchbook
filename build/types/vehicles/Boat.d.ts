a0e0ee1c51cb0053baa0dc829c676683542b563c
Inthenew <38168276+Inthenew@users.noreply.github.com>
2023-12-03 15:43:01 -0500

    Add files via upload

---
+++ build/types/vehicles/Boat.d.ts
@@ -0,0 +1,31 @@
+import * as CANNON from 'cannon';
+import { Vehicle } from './Vehicle';
+import { IControllable } from '../interfaces/IControllable';
+import { EntityType } from '../enums/EntityType';
+export declare class Boat extends Vehicle implements IControllable {
+    entityType: EntityType;
+    drive: string;
+    isBoat: boolean;
+    get speed(): number;
+    private _speed;
+    speed2: number;
+    private steeringWheel;
+    private steeringSimulator;
+    private gear;
+    private shiftTimer;
+    private timeToShift;
+    private characterWantsToExit;
+    su: any;
+    constructor(gltf: any);
+    updateWheelProps(property: any, value: any): void;
+    updateCarSpeed(speed: any): void;
+    noDirectionPressed(): boolean;
+    update(timeStep: number): void;
+    shiftUp(): void;
+    shiftDown(): void;
+    private goForward;
+    physicsPreStep(body: CANNON.Body, car: Boat): void;
+    onInputChange(): void;
+    inputReceiverInit(): void;
+    readCarData(gltf: any): void;
+}
