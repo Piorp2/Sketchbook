eb124c4fae72a2f14284c4c488455af20218a382
Inthenew <38168276+Inthenew@users.noreply.github.com>
2023-12-07 13:50:53 -0500

    Delete build directory

--- build/types/characters/character_states/SwimIdle.d.ts
+++
@@ -1,7 +0,0 @@
-import { CharacterStateBase } from './_stateLibrary';
-import { ICharacterState } from '../../interfaces/ICharacterState';
-import { Character } from '../Character';
-export declare class SwimIdle extends CharacterStateBase implements ICharacterState {
-    constructor(character: Character);
-    update(timeStep: number): void;
-}
